871f54eafff1cff0bdadfeefcb7d1114406e09ca
Martin Oppitz <6279703+deleonio@users.noreply.github.com>
2024-02-20 09:11:05 +0100

    chore: update theme demo

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		project: 'tsconfig.json',
@@ -6,5 +7,30 @@ module.exports = {
 		tsconfigRootDir: __dirname,
 	},
 	plugins: ['@typescript-eslint'],
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	rules: {
+		/**
+		 * Import types with `import type` instead of `import`.
+		 */
+		'@typescript-eslint/consistent-type-imports': 'warn',
+		/**
+		 * This rule is disabled because it is not possible to use the
+		 * `no-unsafe-assignment` rule without breaking the build.
+		 */
+		// '@typescript-eslint/no-unsafe-assignment': 'warn',
+
+		/**
+		 * This setting is necessary because required and optional properties
+		 * and states build on each other in API design. If duplicate or redundant
+		 * types were not used, changes to base types would not be propagated
+		 * and would lead to errors.
+		 */
+		'@typescript-eslint/no-duplicate-type-constituents': 'off',
+		'@typescript-eslint/no-redundant-type-constituents': 'off',
+
+		/**
+		 * The HTML templates in TSX are recognized as any.
+		 */
+		'@typescript-eslint/no-unsafe-member-access': 'off',
+		'@typescript-eslint/no-unsafe-return': 'off',
+	},
 };
